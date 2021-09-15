317bc8922524795f974871f176adf73a3ae6cec4
NickLiffen <nickliffen@gmail.com>
2021-09-15 15:14:39 +0100

    adding new aws feilds

--- functions/createDBRecord/src/main.ts
+++ functions/createDBRecord/src/main.ts
@@ -2,8 +2,6 @@ import { EventBridgeEvent } from "aws-lambda";
 
 import { CodeScanningAlertCreatedEvent } from "@octokit/webhooks-types";
 
-import { v4 as uuidv4 } from 'uuid';
-
 export const handler =  (
   event: EventBridgeEvent<"transaction", CodeScanningAlertCreatedEvent>
 ): Response => {
@@ -13,12 +11,12 @@ export const handler =  (
 
   const response =  { 
     record: {
-      id: uuidv4() as string,
+      id: `${repository.full_name}/${alert.number}` as string,
       alertID: alert.number as number,
       alertURL: alert.html_url as string,
       alertCreatedAtFullTimestamp: alert.created_at as string,
       repositoryName: repository.name as string,
-      organisationName: (organization ? organization.login : "") as string,
+      organisationName: (organization ? organization.login : repository.owner.login) as string,
       alertCreatedAtYear: newDate.getUTCFullYear() as number,
       alertCreatedAtMonth: newDate.getUTCMonth() as number,
       alertCreatedAtDate: newDate.getUTCDate() as number,
