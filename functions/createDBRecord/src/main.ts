18c24878ee12a10dd3af1e62fd32a608181e3916
NickLiffen <nickliffen@gmail.com>
2021-09-15 14:11:11 +0100

    created first data structure

--- functions/createDBRecord/src/main.ts
+++ functions/createDBRecord/src/main.ts
@@ -1,8 +1,30 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { EventBridgeEvent } from "aws-lambda";
+
+import { CodeScanningAlertCreatedEvent } from "@octokit/webhooks-types";
+
+import { v4 as uuidv4 } from 'uuid';
 
 export const handler =  (
-  event: APIGatewayProxyEventV2
-): APIGatewayProxyResultV2 => {
-  console.log(event);
-  return 'success'
+  event: EventBridgeEvent<"transaction", CodeScanningAlertCreatedEvent>
+): Response => {
+  const { detail: { alert, repository, organization} } = event; 
+
+  const newDate = new Date(alert.created_at) as Date;
+
+  const response =  { 
+    record: {
+      id: uuidv4() as string,
+      alertID: alert.number as number,
+      alertURL: alert.html_url as string,
+      alertCreatedAtFullTimestamp: alert.created_at as string,
+      repositoryName: repository.name as string,
+      organisationName: (organization ? organization.login : "") as string,
+      alertCreatedAtYear: newDate.getUTCFullYear() as number,
+      alertCreatedAtMonth: newDate.getUTCMonth() as number,
+      alertCreatedAtDate: newDate.getUTCDate() as number,
+      alertClosedAtFullTimestamp: null,
+    }
+  } as Response
+
+  return response as Response;
 };
--- functions/createDBRecord/types/common/main.d.ts
+++ functions/createDBRecord/types/common/main.d.ts
@@ -1,1 +1,16 @@
+type record = {
+    id: string;
+    alertID: number;
+    alertURL: string;
+    alertCreatedAtFullTimestamp: string;
+    repositoryName: string;
+    organisationName: string;
+    alertCreatedAtYear: number;
+    alertCreatedAtMonth: number;
+    alertCreatedAtDate: number;
+    alertClosedAtFullTimestamp: null;
+};
 
+type Response = {
+    record: record;
+    }
