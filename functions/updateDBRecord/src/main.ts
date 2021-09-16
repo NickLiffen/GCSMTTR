0cf314c1f46a47e6612ae1dd3d5d6bf33845d0f0
NickLiffen <nickliffen@gmail.com>
2021-09-16 11:37:11 +0100

    run update

--- functions/updateDBRecord/src/main.ts
+++ functions/updateDBRecord/src/main.ts
@@ -1,8 +1,39 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { APIGatewayProxyResultV2 } from "aws-lambda";
 
-export const handler =  (
-  event: APIGatewayProxyEventV2
-): APIGatewayProxyResultV2 => {
+import { CodeScanningAlertCreatedEvent } from "@octokit/webhooks-types";
+
+import {
+  DynamoDBClient,
+  DynamoDBClientConfig,
+  GetItemCommand,
+  GetItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+
+export const handler = async (
+  event: CodeScanningAlertCreatedEvent
+): Promise<APIGatewayProxyResultV2> => {
   console.log(event);
-  return 'success'
+
+  const { alert, repository } = event;
+
+  const key = `${repository.full_name}/${alert.number}`;
+
+  const config = {
+    region: process.env.REGION,
+  } as DynamoDBClientConfig;
+
+  const input = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      id: {
+        S: `${key}`,
+      },
+    },
+  } as GetItemCommandInput;
+
+  const client = new DynamoDBClient(config);
+  const command = new GetItemCommand(input);
+  const response = await client.send(command);
+  console.log(response);
+  return "success";
 };
