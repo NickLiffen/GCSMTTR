9159b0d1feaded09aeb8af5724c8dd3260e9bffe
NickLiffen <nickliffen@gmail.com>
2021-09-17 11:59:34 +0100

    Seeing the data I get in the stream

--- functions/handleDBRecordEvent/src/main.ts
+++ functions/handleDBRecordEvent/src/main.ts
@@ -1,8 +1,24 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import { DynamoDBStreamEvent, APIGatewayProxyResultV2 } from "aws-lambda";
 
-export const handler =  (
-  event: APIGatewayProxyEventV2
+export const handler = (
+  event: DynamoDBStreamEvent
 ): APIGatewayProxyResultV2 => {
-  console.log(event);
-  return 'success'
+  const { Records } = event;
+
+  const { eventName, dynamodb } = Records[0];
+
+  console.log(eventName);
+
+  console.log(dynamodb);
+
+  const d = new Date();
+  const month = d.getMonth().toString();
+  const year = d.getMonth().toString();
+  const regExp = /^0[0-9].*$/;
+  const qualifiedMonth = regExp.test(month) ? month : `0${month}`;
+  const monthyPeriod = `${year}-${qualifiedMonth}`;
+
+  console.log(monthyPeriod);
+
+  return "success";
 };
