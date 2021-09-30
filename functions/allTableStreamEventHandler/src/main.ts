591a4dee2d6ce0d2e01733023f1bfbbc82d6ab1c
NickLiffen <nickliffen@gmail.com>
2021-09-30 10:47:09 +0100

    Added Queue Information

--- functions/allTableStreamEventHandler/src/main.ts
+++ functions/allTableStreamEventHandler/src/main.ts
@@ -4,25 +4,32 @@ import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 
 import { v4 as uuidv4 } from "uuid";
 
-export const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
-  const { Records } = event;
-
-  const { dynamodb } = Records[0];
-
-  const repositoryName = dynamodb?.NewImage?.repositoryName as string;
-
-  const input = {
-    MessageDeduplicationId: uuidv4(),
-    MessageGroupId: repositoryName,
-    QueueUrl: process.env.QUEUE_URL,
-    MessageBody: JSON.stringify(dynamodb?.NewImage),
-  };
-
-  const client = new SQSClient({ region: process.env.REGION });
-  const command = new SendMessageCommand(input);
-
+export const handler = async (event: DynamoDBStreamEvent): Promise<string> => {
   try {
+    const { Records } = event;
+
+    const { eventName, dynamodb } = Records[0];
+
+    if (eventName === "REMOVE" || !eventName) return "remove event not supported";
+
+    const repositoryName = dynamodb?.NewImage?.repositoryName as string;
+
+    const input = {
+      MessageDeduplicationId: uuidv4(),
+      MessageGroupId: repositoryName,
+      QueueUrl: process.env.QUEUE_URL,
+      MessageBody: JSON.stringify(dynamodb?.NewImage),
+    };
+
+    console.log("input", input);
+    console.log("dynamodb?.NewImage", dynamodb?.NewImage);
+
+    const client = new SQSClient({ region: process.env.REGION });
+    const command = new SendMessageCommand(input);
+
     await client.send(command);
+
+    return "data to send to queue successfully";
   } catch (error) {
     console.log(error);
     throw error;
