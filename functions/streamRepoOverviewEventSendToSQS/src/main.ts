b3e4e41620a1331e60c32914e0211190f5db7c12
NickLiffen <nickliffen@gmail.com>
2021-10-05 13:05:38 +0100

    fixing the queue sending functions

--- functions/streamRepoOverviewEventSendToSQS/src/main.ts
+++ functions/streamRepoOverviewEventSendToSQS/src/main.ts
@@ -6,27 +6,37 @@ import { v4 as uuidv4 } from "uuid";
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<string> => {
   try {
+    let data = {} as QueueData;
+
     const { Records } = event;
 
     const { eventName: ev, dynamodb } = Records[0];
 
-    if (ev === "REMOVE" || !ev) return "remove event not supported";
+    if (ev === "REMOVE" || !ev) {
+      return "remove event not supported";
+    }
+
+    if (ev === "INSERT") {
+      data = { NewImage: dynamodb?.NewImage, EvetName: ev };
+    }
+
+    if (ev === "MODIFY") {
+      data = {
+        NewImage: dynamodb?.NewImage,
+        OldImage: dynamodb?.OldImage,
+        EvetName: ev,
+      };
+    }
+
+    console.log("Data sending to SQS Queue:", data);
 
     const organisationName = dynamodb?.NewImage?.organisationName.S as string;
-
-    const eventName = {
-      S: ev,
-    };
-
-    const merged = { ...dynamodb?.NewImage, ...eventName };
-
-    console.log("merged data:", merged);
 
     const input = {
       MessageDeduplicationId: uuidv4(),
       MessageGroupId: organisationName,
       QueueUrl: process.env.QUEUE_URL,
-      MessageBody: JSON.stringify(merged),
+      MessageBody: JSON.stringify(data),
     };
 
     console.log("input", input);
